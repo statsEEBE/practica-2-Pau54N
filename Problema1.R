b530701878da9accdf767b8f1a24d824ba88a3e9
pau.santana <pau.santana@auleseebe.upc.edu>
2024-10-03 13:21:41 +0200

    Problema 2

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,15 @@
 #Codigo para problema 1
 
+
+R2<- 0.1923
+R <- sqrt(R2)
+R
+m <- 0.06576
+b <- 11.79755
+x <- 647.1
+y_pred <- m*x+b
+
+x_a <- 0:700
+y_pred2 <- m*x_a+b
+y_pred2
+plot(x_a,y_pred2, type='l', ylim = c(0,120))
